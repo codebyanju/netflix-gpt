e487c806cc50e672994256590e224f131e373354
anju <codebyanju@gmail.com>
2025-09-28 21:33:15 +0530

    Project Setup using Vite

---
+++ src/components/Body.js
@@ -0,0 +1,27 @@
+// import Login from "components/Login";
+import Browse from "components/Browse";
+import Login from "components/Login";
+
+import { RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
